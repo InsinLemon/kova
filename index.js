950b9873ae2d41589200070546527a6c4532c6cb
InsinLemon <enochbenson3342@gmail.com>
2025-04-17 22:08:36 +0100

    second commit

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use(express.static('../frontend'));
 
 // Database connection
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'kova-app',
+  host: process.env.DB_HOST || 'cpanel.ecomm.ng',
+  user: process.env.DB_USER || 'ecommng1_enoch',
+  password: process.env.DB_PASSWORD || 'Enoch@0330',
+  database: process.env.DB_NAME || 'ecommng1_kova-app',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
